59b876052774f4f68c0965cbea13bf1bc3e51186
jeff <jeff@swarmgate.com>
2018-06-02 22:39:06 -0700

    fix canvas#2

--- client/src/Canvas_Item/_appendIcon.js
+++ client/src/Canvas_Item/_appendIcon.js
@@ -8,7 +8,16 @@ function _appendIcon(src,option){
     icon._dom.style.left = '42.5%';
     icon._dom.style.top = '45%';
     icon.touchCb = this._handle_icon_select.bind(this);
-    icon.deleteCb = this._deleteCb.bind(this);
+    icon.deleteCb = (del_icon)=>{
+        let icons = [];
+        this._icons.forEach(icon=>{
+            console.log(icon);
+            if(del_icon !== icon)
+                icons.push(icon);
+        });
+        this._icons = icons;
+        this._deleteCb.bind(this);
+    };
 
     icon.appendCb = (icon)=>{
         icon.active = true;
